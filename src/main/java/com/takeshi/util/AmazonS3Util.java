88c226cd42aa0af0b9bf0524a69397f8f4d09430
NanaseTakeshi <xwhuai725@163.com>
2023-05-19 12:12:00 +0800

    构建

--- src/main/java/com/takeshi/util/AmazonS3Util.java
+++ src/main/java/com/takeshi/util/AmazonS3Util.java
@@ -86,44 +86,48 @@ public final class AmazonS3Util {
         if (ObjUtil.isNull(transferManager)) {
             synchronized (AmazonS3Util.class) {
                 if (ObjUtil.isNull(transferManager)) {
-                    // 获取密钥
-                    AWSSecretsManagerCredentials awsSecrets = StaticConfig.takeshiProperties.getAwsSecrets();
-                    BUCKET_NAME = awsSecrets.getBucketName();
-                    AWSSecretsManager awsSecretsManager = AWSSecretsManagerClientBuilder.standard()
-                            .withRegion(awsSecrets.getRegion())
-                            .withCredentials(new AWSStaticCredentialsProvider(awsSecrets))
-                            .build();
-                    GetSecretValueRequest getSecretValueRequest = new GetSecretValueRequest();
-                    getSecretValueRequest.setSecretId(awsSecrets.getSecretId());
-                    GetSecretValueResult getSecretValueResult = awsSecretsManager.getSecretValue(getSecretValueRequest);
-                    String secret = StrUtil.isNotBlank(getSecretValueResult.getSecretString()) ? getSecretValueResult.getSecretString() : new String(java.util.Base64.getDecoder().decode(getSecretValueResult.getSecretBinary()).array());
-                    SECRET = GsonUtil.fromJson(secret, awsSecrets.getSecretClass());
-                    JSON_NODE = new ObjectMapper().valueToTree(secret);
-                    String accessKey = JSON_NODE.get(awsSecrets.getAccessKeyName()).asText();
-                    String secretKey = JSON_NODE.get(awsSecrets.getSecretKeyName()).asText();
-                    // S3
-                    AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard()
-                            .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
-                            .withRegion(awsSecrets.getRegion())
-                            .build();
-                    if (!amazonS3.doesBucketExistV2(BUCKET_NAME)) {
-                        // 创建桶
-                        amazonS3.createBucket(BUCKET_NAME);
-                        // 设置生命周期规则，指示自生命周期启动后必须经过7天才能中止并删除不完整的分段上传
-                        BucketLifecycleConfiguration.Rule lifecycleRule = new BucketLifecycleConfiguration.Rule()
-                                .withId("Automatically delete incomplete multipart upload after seven days")
-                                .withAbortIncompleteMultipartUpload(new AbortIncompleteMultipartUpload().withDaysAfterInitiation(7))
-                                .withStatus(BucketLifecycleConfiguration.ENABLED);
-                        // 将生命周期规则设置到桶中
-                        amazonS3.setBucketLifecycleConfiguration(BUCKET_NAME, new BucketLifecycleConfiguration().withRules(lifecycleRule));
-                        // 设置跨域规则
-                        CORSRule corsRule = new CORSRule().withAllowedMethods(Collections.singletonList(CORSRule.AllowedMethods.GET)).withAllowedOrigins(Collections.singletonList("*"));
-                        // 将跨域规则设置到桶中
-                        amazonS3.setBucketCrossOriginConfiguration(BUCKET_NAME, new BucketCrossOriginConfiguration().withRules(corsRule));
-                        // 为指定的存储桶启用传输加速
-                        amazonS3.setBucketAccelerateConfiguration(new SetBucketAccelerateConfigurationRequest(BUCKET_NAME, new BucketAccelerateConfiguration(BucketAccelerateStatus.Enabled)));
+                    try {
+                        // 获取密钥
+                        AWSSecretsManagerCredentials awsSecrets = StaticConfig.takeshiProperties.getAwsSecrets();
+                        BUCKET_NAME = awsSecrets.getBucketName();
+                        AWSSecretsManager awsSecretsManager = AWSSecretsManagerClientBuilder.standard()
+                                .withRegion(awsSecrets.getRegion())
+                                .withCredentials(new AWSStaticCredentialsProvider(awsSecrets))
+                                .build();
+                        GetSecretValueRequest getSecretValueRequest = new GetSecretValueRequest();
+                        getSecretValueRequest.setSecretId(awsSecrets.getSecretId());
+                        GetSecretValueResult getSecretValueResult = awsSecretsManager.getSecretValue(getSecretValueRequest);
+                        String secret = StrUtil.isNotBlank(getSecretValueResult.getSecretString()) ? getSecretValueResult.getSecretString() : new String(java.util.Base64.getDecoder().decode(getSecretValueResult.getSecretBinary()).array());
+                        SECRET = GsonUtil.fromJson(secret, awsSecrets.getSecretClass());
+                        JSON_NODE = new ObjectMapper().readValue(secret, JsonNode.class);
+                        String accessKey = JSON_NODE.get(awsSecrets.getAccessKeyName()).asText();
+                        String secretKey = JSON_NODE.get(awsSecrets.getSecretKeyName()).asText();
+                        // S3
+                        AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard()
+                                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
+                                .withRegion(awsSecrets.getRegion())
+                                .build();
+                        if (!amazonS3.doesBucketExistV2(BUCKET_NAME)) {
+                            // 创建桶
+                            amazonS3.createBucket(BUCKET_NAME);
+                            // 设置生命周期规则，指示自生命周期启动后必须经过7天才能中止并删除不完整的分段上传
+                            BucketLifecycleConfiguration.Rule lifecycleRule = new BucketLifecycleConfiguration.Rule()
+                                    .withId("Automatically delete incomplete multipart upload after seven days")
+                                    .withAbortIncompleteMultipartUpload(new AbortIncompleteMultipartUpload().withDaysAfterInitiation(7))
+                                    .withStatus(BucketLifecycleConfiguration.ENABLED);
+                            // 将生命周期规则设置到桶中
+                            amazonS3.setBucketLifecycleConfiguration(BUCKET_NAME, new BucketLifecycleConfiguration().withRules(lifecycleRule));
+                            // 设置跨域规则
+                            CORSRule corsRule = new CORSRule().withAllowedMethods(Collections.singletonList(CORSRule.AllowedMethods.GET)).withAllowedOrigins(Collections.singletonList("*"));
+                            // 将跨域规则设置到桶中
+                            amazonS3.setBucketCrossOriginConfiguration(BUCKET_NAME, new BucketCrossOriginConfiguration().withRules(corsRule));
+                            // 为指定的存储桶启用传输加速
+                            amazonS3.setBucketAccelerateConfiguration(new SetBucketAccelerateConfigurationRequest(BUCKET_NAME, new BucketAccelerateConfiguration(BucketAccelerateStatus.Enabled)));
+                        }
+                        transferManager = TransferManagerBuilder.standard().withS3Client(amazonS3).build();
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
                     }
-                    transferManager = TransferManagerBuilder.standard().withS3Client(amazonS3).build();
                 }
             }
         }
