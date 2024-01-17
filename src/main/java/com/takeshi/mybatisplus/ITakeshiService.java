7259955685715d5391457b116d13969d5d1e1133
Nanase Takeshi <xwhuai725@163.com>
2024-01-17 15:36:19 +0800

    优化

--- src/main/java/com/takeshi/mybatisplus/ITakeshiService.java
+++ src/main/java/com/takeshi/mybatisplus/ITakeshiService.java
@@ -226,6 +226,17 @@ public interface ITakeshiService<T> extends IService<T> {
      * @param column 查询的字段
      * @param val    查询的值
      * @param retBO  异常信息对象
+     */
+    default void columnExists(SFunction<T, ?> column, Object val, RetBO retBO) {
+        this.getBaseMapper().columnExists(column, val, retBO);
+    }
+
+    /**
+     * 判断当前实体对象中某个字段值是否已存在，已存在时抛出异常
+     *
+     * @param column 查询的字段
+     * @param val    查询的值
+     * @param retBO  异常信息对象
      * @param args   将为消息中的参数填充的参数数组（参数在消息中类似于“{0}”、“{1,date}”、“{2,time}”），如果没有则为null
      */
     default void columnExists(SFunction<T, ?> column, Object val, RetBO retBO, Object... args) {
--- src/main/java/com/takeshi/mybatisplus/TakeshiMapper.java
+++ src/main/java/com/takeshi/mybatisplus/TakeshiMapper.java
@@ -220,6 +220,19 @@ public interface TakeshiMapper<T> extends BaseMapper<T> {
      * @param column 查询的字段
      * @param val    查询的值
      * @param retBO  异常信息对象
+     */
+    default void columnExists(SFunction<T, ?> column, Object val, RetBO retBO) {
+        if (this.exists(Wrappers.lambdaQuery(this.getEntityClass()).eq(column, val))) {
+            throw new TakeshiException(retBO);
+        }
+    }
+
+    /**
+     * 判断当前实体对象中某个字段值是否已存在，已存在时抛出异常
+     *
+     * @param column 查询的字段
+     * @param val    查询的值
+     * @param retBO  异常信息对象
      * @param args   将为消息中的参数填充的参数数组（参数在消息中类似于“{0}”、“{1,date}”、“{2,time}”），如果没有则为null
      */
     default void columnExists(SFunction<T, ?> column, Object val, RetBO retBO, Object... args) {
