f5eb6641d8e53ae11717c46980c54254a6440a4e
Nanase Takeshi <xwhuai725@163.com>
2024-04-02 14:25:45 +0800

    优化

--- src/main/java/com/takeshi/exception/TakeshiException.java
+++ src/main/java/com/takeshi/exception/TakeshiException.java
@@ -37,10 +37,10 @@ public class TakeshiException extends RuntimeException {
     /**
      * 异常
      *
-     * @param message  消息
      * @param metaData 附加数据
+     * @param message  消息
      */
-    public TakeshiException(String message, Object metaData) {
+    public TakeshiException(Object metaData, String message) {
         super(GsonUtil.toJson(ResponseData.fail(message).setMetaData(metaData)));
     }
 
@@ -57,11 +57,11 @@ public class TakeshiException extends RuntimeException {
     /**
      * 异常
      *
+     * @param metaData 附加数据
      * @param code     状态码
      * @param message  消息
-     * @param metaData 附加数据
      */
-    public TakeshiException(int code, String message, Object metaData) {
+    public TakeshiException(Object metaData, int code, String message) {
         super(GsonUtil.toJson(ResponseData.retData(code, message).setMetaData(metaData)));
     }
 
@@ -77,10 +77,10 @@ public class TakeshiException extends RuntimeException {
     /**
      * 异常
      *
-     * @param retBO    消息
      * @param metaData 附加数据
+     * @param retBO    消息
      */
-    public TakeshiException(RetBO retBO, Object metaData) {
+    public TakeshiException(Object metaData, RetBO retBO) {
         super(GsonUtil.toJson(ResponseData.retData(retBO).setMetaData(metaData)));
     }
 
@@ -97,11 +97,11 @@ public class TakeshiException extends RuntimeException {
     /**
      * 异常
      *
-     * @param retBO    消息
      * @param metaData 附加数据
+     * @param retBO    消息
      * @param args     将为消息中的参数填充的参数数组（参数在消息中类似于“{0}”、“{1,date}”、“{2,time}”），如果没有则为null
      */
-    public TakeshiException(RetBO retBO, Object metaData, Object... args) {
+    public TakeshiException(Object metaData, RetBO retBO, Object... args) {
         super(GsonUtil.toJson(ResponseData.retData(retBO, args).setMetaData(metaData)));
     }
 
@@ -127,12 +127,12 @@ public class TakeshiException extends RuntimeException {
     /**
      * 包装成Supplier
      *
-     * @param message  消息
      * @param metaData 附加数据
+     * @param message  消息
      * @return Supplier
      */
     public static Supplier<TakeshiException> supplier(String message, Object metaData) {
-        return () -> new TakeshiException(message, metaData);
+        return () -> new TakeshiException(metaData, message);
     }
 
     /**
@@ -149,13 +149,13 @@ public class TakeshiException extends RuntimeException {
     /**
      * 包装成Supplier
      *
+     * @param metaData 附加数据
      * @param code     状态码
      * @param message  消息
-     * @param metaData 附加数据
      * @return Supplier
      */
-    public static Supplier<TakeshiException> supplier(int code, String message, Object metaData) {
-        return () -> new TakeshiException(code, message, metaData);
+    public static Supplier<TakeshiException> supplier(Object metaData, int code, String message) {
+        return () -> new TakeshiException(metaData, code, message);
     }
 
     /**
@@ -171,12 +171,12 @@ public class TakeshiException extends RuntimeException {
     /**
      * 包装成Supplier
      *
-     * @param retBO    消息
      * @param metaData 附加数据
+     * @param retBO    消息
      * @return Supplier
      */
-    public static Supplier<TakeshiException> supplier(RetBO retBO, Object metaData) {
-        return () -> new TakeshiException(retBO, metaData);
+    public static Supplier<TakeshiException> supplier(Object metaData, RetBO retBO) {
+        return () -> new TakeshiException(metaData, retBO);
     }
 
     /**
@@ -193,13 +193,13 @@ public class TakeshiException extends RuntimeException {
     /**
      * 包装成Supplier
      *
-     * @param retBO    消息
      * @param metaData 附加数据
+     * @param retBO    消息
      * @param args     将为消息中的参数填充的参数数组（参数在消息中类似于“{0}”、“{1,date}”、“{2,time}”），如果没有则为null
      * @return Supplier
      */
-    public static Supplier<TakeshiException> supplier(RetBO retBO, Object metaData, Object... args) {
-        return () -> new TakeshiException(retBO, metaData, args);
+    public static Supplier<TakeshiException> supplier(Object metaData, RetBO retBO, Object... args) {
+        return () -> new TakeshiException(metaData, retBO, args);
     }
 
     /**
