449e3aeeced3651befca02e6937aa36f83d13061
takeshi <xwhuai@gmail.com>
2024-09-27 09:53:00 +0800

    开发

--- src/main/java/com/takeshi/mybatisplus/typehandler/GeoPointTypeHandler.java
+++ src/main/java/com/takeshi/mybatisplus/typehandler/GeoPointTypeHandler.java
@@ -2,24 +2,22 @@ package com.takeshi.mybatisplus.typehandler;
 
 import cn.hutool.core.util.ArrayUtil;
 import com.takeshi.pojo.bo.GeoPointBO;
+import lombok.SneakyThrows;
 import org.apache.ibatis.type.BaseTypeHandler;
 import org.apache.ibatis.type.JdbcType;
 import org.locationtech.jts.geom.Coordinate;
-import org.locationtech.jts.geom.CoordinateXY;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Point;
-import org.locationtech.jts.geom.impl.CoordinateArraySequence;
-import org.locationtech.jts.io.ByteArrayInStream;
 import org.locationtech.jts.io.ByteOrderValues;
 import org.locationtech.jts.io.WKBReader;
 import org.locationtech.jts.io.WKBWriter;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * <p>GeoPointTypeHandler</p>
@@ -31,9 +29,30 @@ import java.util.stream.Collectors;
  */
 public class GeoPointTypeHandler extends BaseTypeHandler<GeoPointBO> {
 
+    /**
+     * geometry factory
+     */
+    public final static GeometryFactory GEOMETRY_FACTORY = new GeometryFactory();
+
+    /**
+     * wkb writer
+     */
+    public final static WKBWriter WKB_WRITER = new WKBWriter(2, ByteOrderValues.LITTLE_ENDIAN);
+
+    /**
+     * wkb reader
+     */
+    public final static WKBReader WKB_READER = new WKBReader();
+
     @Override
     public void setNonNullParameter(PreparedStatement preparedStatement, int i, GeoPointBO geoPointBO, JdbcType jdbcType) throws SQLException {
-        preparedStatement.setBytes(i, toWkb(geoPointBO));
+        Point point = GEOMETRY_FACTORY.createPoint(new Coordinate(geoPointBO.getLon(), geoPointBO.getLat()));
+        byte[] bytes = WKB_WRITER.write(point);
+        byte[] wkb = new byte[bytes.length + 4];
+        // 设置SRID=4326，也就是谷歌地图WGS84坐标系
+        ByteOrderValues.putInt(4326, wkb, ByteOrderValues.LITTLE_ENDIAN);
+        System.arraycopy(bytes, 0, wkb, 4, bytes.length);
+        preparedStatement.setBytes(i, wkb);
     }
 
     @Override
@@ -51,49 +70,14 @@ public class GeoPointTypeHandler extends BaseTypeHandler<GeoPointBO> {
         return this.geoPointFromBytes(callableStatement.getBytes(i));
     }
 
-    /**
-     * 查询数据库时也直接传toWkb后的byte数组进行查询即可
-     *
-     * @param geoPointBO geoPointBO
-     * @return byte[]
-     */
-    public static byte[] toWkb(GeoPointBO geoPointBO) {
-        // 转Geometry
-        CoordinateArraySequence coordinateArraySequence = new CoordinateArraySequence(new Coordinate[]{new CoordinateXY(geoPointBO.getLon(), geoPointBO.getLat())});
-        Point point = new Point(coordinateArraySequence, new GeometryFactory());
-        // Geometry转WKB
-        byte[] geometryBytes = new WKBWriter(2, ByteOrderValues.LITTLE_ENDIAN, false).write(point);
-        // 设置SRID为mysql默认的 0
-        byte[] wkb = new byte[geometryBytes.length + 4];
-        wkb[0] = wkb[1] = wkb[2] = wkb[3] = 0;
-        System.arraycopy(geometryBytes, 0, wkb, 4, geometryBytes.length);
-        return wkb;
-    }
-
-    /**
-     * 查询数据库时也直接传toWkb后的byte数组进行查询即可
-     *
-     * @param geoPointBOList geoPointBOList
-     * @return List
-     */
-    public static List<byte[]> toWkb(List<GeoPointBO> geoPointBOList) {
-        return geoPointBOList.stream().map(GeoPointTypeHandler::toWkb).collect(Collectors.toList());
-    }
-
-    private GeoPointBO geoPointFromBytes(byte[] bytes) {
-        try {
-            if (ArrayUtil.isEmpty(bytes)) {
-                return null;
-            }
-            ByteArrayInStream byteArrayInStream = new ByteArrayInStream(bytes);
-            // 字节数组前4个字节表示SRID去掉
-            byteArrayInStream.read(new byte[4]);
-            WKBReader wkbReader = new WKBReader();
-            Point point = wkbReader.read(byteArrayInStream).getInteriorPoint();
-            return new GeoPointBO(point.getX(), point.getY());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+    @SneakyThrows
+    private GeoPointBO geoPointFromBytes(byte[] wkb) {
+        if (ArrayUtil.isEmpty(wkb)) {
+            return null;
         }
+        byte[] bytes = ByteBuffer.allocate(wkb.length - 4).order(ByteOrder.LITTLE_ENDIAN).put(wkb, 4, wkb.length - 4).array();
+        Point point = WKB_READER.read(bytes).getInteriorPoint();
+        return new GeoPointBO(point.getX(), point.getY());
     }
 
 }
