73a62da863b8f7ee8cc8932c61480e6fbad3f67e
takeshi <xwhuai@gmail.com>
2025-02-18 17:39:49 +0800

    优化

--- src/main/java/com/takeshi/mybatisplus/typehandler/InetAddressTypeHandler.java
+++ src/main/java/com/takeshi/mybatisplus/typehandler/InetAddressTypeHandler.java
@@ -1,12 +1,10 @@
 package com.takeshi.mybatisplus.typehandler;
 
-import cn.hutool.core.net.Ipv4Util;
-import cn.hutool.core.util.ObjUtil;
+import cn.hutool.core.util.ArrayUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.type.BaseTypeHandler;
 import org.apache.ibatis.type.JdbcType;
 
-import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.sql.CallableStatement;
@@ -15,26 +13,24 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
- * <p>Ipv4TypeHandler</p>
+ * <p>InetAddressTypeHandler</p>
  * <p>注意！！ 使用typeHandler，必须开启autoResultMap映射注解</p>
  * <p>@TableName(autoResultMap = true)</p>
- * <p>@TableField(typeHandler = Ipv4TypeHandler.class)</p>
- * <p>根据ip地址(xxx.xxx.xxx.xxx)计算出long型的数据</p>
- * <p>存入数据库，从数据库中获取的时候也转成ip v4地址：xx.xx.xx.xx返回到实体类中</p>
+ * <p>@TableField(typeHandler = InetAddressTypeHandler.class)</p>
+ * <p>根据ipv4/ipv6地址得出字节数组</p>
+ * <p>存入数据库，从数据库中获取的时候也转成ipv4/ipv6地址返回到实体类中</p>
  *
  * @author 七濑武【Nanase Takeshi】
  */
 @Slf4j
-public class Ipv4TypeHandler extends BaseTypeHandler<String> {
+public class InetAddressTypeHandler extends BaseTypeHandler<String> {
 
     @Override
     public void setNonNullParameter(PreparedStatement ps, int i, String parameter, JdbcType jdbcType)
             throws SQLException {
         try {
             InetAddress inetAddress = InetAddress.getByName(parameter);
-            if (inetAddress instanceof Inet4Address) {
-                ps.setLong(i, Ipv4Util.ipv4ToLong(parameter));
-            }
+            ps.setBytes(i, inetAddress.getAddress());
         } catch (UnknownHostException e) {
             log.error("Ipv4TypeHandler.setNonNullParameter --> e: ", e);
         }
@@ -42,21 +38,28 @@ public class Ipv4TypeHandler extends BaseTypeHandler<String> {
 
     @Override
     public String getNullableResult(ResultSet rs, String columnName) throws SQLException {
-        return this.longToIpv4(rs.getLong(columnName));
+        return this.getIpAddress(rs.getBytes(columnName));
     }
 
     @Override
     public String getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
-        return this.longToIpv4(rs.getLong(columnIndex));
+        return this.getIpAddress(rs.getBytes(columnIndex));
     }
 
     @Override
     public String getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
-        return this.longToIpv4(cs.getLong(columnIndex));
+        return this.getIpAddress(cs.getBytes(columnIndex));
     }
 
-    private String longToIpv4(long value) {
-        return ObjUtil.isNotNull(value) ? Ipv4Util.longToIpv4(value) : null;
+    private String getIpAddress(byte[] value) {
+        if (ArrayUtil.isNotEmpty(value)) {
+            try {
+                return InetAddress.getByAddress(value).getHostAddress();
+            } catch (UnknownHostException e) {
+                log.error("Ipv4TypeHandler.getIpAddress --> e: ", e);
+            }
+        }
+        return null;
     }
 
 }
